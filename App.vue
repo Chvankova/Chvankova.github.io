882d8f4a8b87332decb7706bd9ccfe128bad0c77
Chvankova <85752871+Chvankova@users.noreply.github.com>
2023-12-30 20:31:37 +0100

    1

--- App.vue
+++ App.vue
@@ -1,3 +1,18 @@
 <template>
-  <h2>Hy,Sandri!!!</h2>
+  <div>
+    <h1>Hello, Vue.js with Bootstrap!</h1>
+    <p>This is a simple Vue.js component using Bootstrap classes.</p>
+  </div>
 </template>
+
+<script>
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'App'
+});
+</script>
+
+<style>
+/* Add your custom styles or Bootstrap classes here */
+</style>
--- app.js
+++ app.js
@@ -1,5 +1,27 @@
+// import {createApp} from Vue
+// const app = Vue
 
-import App from './App.vue'
-const app = Vue.createApp(App)
+// createApp({
+//   data() {
+//     return {
+//       name: 'Sandra',
+//       birthDate: 1982,
+//       year: null
+//     } 
+//   },
+//   methods: {
+//    age(){
+//     this.year - this.birthDate
+//    }
+//   }
+  
+// }).mount("#app");
 
-app.mount("#app")
+// main.js
+
+import { createApp } from 'vue';
+import App from './App.vue';
+
+const app = createApp(App);
+
+app.mount('#app');
